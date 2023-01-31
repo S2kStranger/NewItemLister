68f4ecf3595ad471c4e6b804de042ac5c4dbb794
S2kStranger <s2kkasera@gmail.com>
2023-01-31 19:41:56 +0530

    Use of queryselector and queryselectorall

--- demo.js
+++ demo.js
@@ -12,3 +12,22 @@ for(var i=0;i<listitem.length;i++)
 }
 var li=document.getElementsByTagName('li');
 li[4].style.fontWeight='bold';
+
+//Use of Query Selector
+var seconditem=document.querySelector("li:nth-child(2)");
+// seconditem.style.backgroundColor='green';
+var thirditem=document.querySelector("li:nth-child(3)");
+// thirditem.style.display='none';
+
+//Use of querySelectorAll
+
+var liitem=document.querySelectorAll(".list-group-item");
+liitem[1].style.color='green';
+var odditem=document.querySelectorAll('li:nth-child(odd)');
+console.log(odditem);
+for(var i=0;i<odditem.length;i++)
+{
+    odditem[i].style.backgroundColor='green';
+}
+
+
