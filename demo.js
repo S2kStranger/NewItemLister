532c88f91b5de78a168714b3d87034acb56c4b8c
S2kStranger <s2kkasera@gmail.com>
2023-01-31 18:15:45 +0530

    modifiedjs

--- demo.js
+++ demo.js
@@ -1,1 +1,6 @@
-console.log("Pushing into Github");
+var headertitle=document.getElementById('header-title');
+headertitle.innerText="Item Listener";
+document.getElementById('main-header').style.borderBottom= 'solid 3px #000';
+var additem=document.getElementsByClassName('title');
+additem[0].style.fontWeight = "bold";
+additem[0].style.color = "green";
