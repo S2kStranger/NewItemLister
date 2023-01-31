c391a62c8a8a832501fae04b9b453d29f682c0e3
S2kStranger <s2kkasera@gmail.com>
2023-01-31 19:00:24 +0530

    Modified list items

--- demo.js
+++ demo.js
@@ -4,3 +4,9 @@ document.getElementById('main-header').style.borderBottom= 'solid 3px #000';
 var additem=document.getElementsByClassName('title');
 additem[0].style.fontWeight = "bold";
 additem[0].style.color = "green";
+var listitem=document.getElementsByClassName('list-group-item');
+listitem[2].style.backgroundColor ='green';
+for(var i=0;i<listitem.length;i++)
+{
+    listitem[i].style.fontWeight='bold';
+}
